d57053cd463d273939798159f9d5ecb5051e0bcc
Harsh Agarwal <harsh.agarwal.eee14@itbhu.ac.in>
2019-02-17 14:38:28 -0500

    Update Resampling.py

--- Resampling.py
+++ Resampling.py
@@ -39,10 +39,24 @@ class Resampling:
         """
 
         weights = X_bar[:, 3]
-        total_weights = np.sum(weights)
-        std = np.std(weights)
-        print('Total particle weights {}'.format(total_weights))
-        print('Weights standard deviation {}'.format(std))
+        num_particles = X_bar.shape[0]
+        X_bar_resampled = np.zeros_like(X_bar)
+        r = np.random.rand() * (1.0/num_particles)
+        c = weights[0]
+        i = 1
+
+        for m in range(0,num_particles):
+            u = r + (m-1)*(1.0/num_particles)
+            while(u > c):
+                i = i + 1
+                c = c + weights[i]
+
+            X_bar_resampled[m,:] =  X_bar[i,:]
+            
+        # total_weights = np.sum(weights)
+        # std = np.std(weights)
+        # print('Total particle weights {}'.format(total_weights))
+        # print('Weights standard deviation {}'.format(std))
 
         return X_bar_resampled
 
@@ -51,3 +65,57 @@ class Resampling:
 
 if __name__ == "__main__":
     pass
+
+# import numpy as np
+# import pdb
+# import matplotlib.pyplot as plt
+# from scipy.stats import norm
+
+# class Resampling:
+
+#     """
+#     References: Thrun, Sebastian, Wolfram Burgard, and Dieter Fox. Probabilistic robotics. MIT press, 2005.
+#     [Chapter 4.3]
+#     """
+
+#     def __init__(self):
+#         """
+#         TODO : Initialize resampling process parameters here
+#         """
+
+#     def multinomial_sampler(self, X_bar):
+
+#         """
+#         param[in] X_bar : [num_particles x 4] sized array containing [x, y, theta, wt] values for all particles
+#         param[out] X_bar_resampled : [num_particles x 4] sized array containing [x, y, theta, wt] values for resampled set of particles
+#         """
+
+#         weights = X_bar[:, 3]
+#         total_num = len(weights)
+
+#         weights = weights / np.sum(weights)
+#         resampled_index = np.random.multinomial(self.dice_count*total_num, weights, size=1)[0, :]
+#         X_bar_resampled = X_bar[resampled_index, :]
+
+#         return X_bar_resampled
+
+#     def low_variance_sampler(self, X_bar):
+
+#         """
+#         param[in] X_bar : [num_particles x 4] sized array containing [x, y, theta, wt] values for all particles
+#         param[out] X_bar_resampled : [num_particles x 4] sized array containing [x, y, theta, wt] values for resampled set of particles
+#         """
+
+#         weights = X_bar[:, 3]
+#         total_weights = np.sum(weights)
+#         std = np.std(weights)
+#         print('Total particle weights {}'.format(total_weights))
+#         print('Weights standard deviation {}'.format(std))
+
+#         return X_bar_resampled
+
+#     def add_particle(self, X_bar, weight_threshold):
+#         pass
+
+# if __name__ == "__main__":
+#     pass
