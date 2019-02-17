1de7c087715ee783536b9166df8da7de1b671729
Tejas Khot <tkhot@andrew.cmu.edu>
2019-02-17 14:55:31 -0500

    reorganize code; first full run works

--- code/main.py
+++ code/main.py
@@ -154,7 +154,7 @@ def main():
         """
         RESAMPLING
         """
-        # X_bar = resampler.low_variance_sampler(X_bar)
+        X_bar = resampler.low_variance_sampler(X_bar)
 
         if vis_flag:
             visualize_timestep(X_bar, time_idx)
